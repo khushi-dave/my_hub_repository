419fefb2f568381e61bc2fba91e6ac5ef345e337
khushi-dave <68141662+khushi-dave@users.noreply.github.com>
2023-05-05 11:25:29 +0530

    Create hubconf.py

---
+++ yolov8/hubconf.py
@@ -0,0 +1,11 @@
+dependencies = ['torch', 'numpy']
+
+from ultralytics.yolo import YOLO
+
+def yolov8(pretrained=False, **kwargs):
+    """YOLOv8 model with pretrained weights"""
+    model = YOLO(cfg='models/yolov8.cfg', ch=3, nc=80)
+    if pretrained:
+        state_dict = torch.load('yolov8.pt', map_location='cpu')['model']
+        model.load_state_dict(state_dict)
+    return model
